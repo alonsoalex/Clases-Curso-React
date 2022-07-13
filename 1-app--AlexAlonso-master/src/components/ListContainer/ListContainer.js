53f925f19c0a78f4c499614b1d538cf8497ab0b3
alonsoalex <98624661+alonsoalex@users.noreply.github.com>
2022-07-13 12:22:17 -0300

    reEntrega

--- 1-app--AlexAlonso-master/src/components/ListContainer/ListContainer.js
+++ 1-app--AlexAlonso-master/src/components/ListContainer/ListContainer.js
@@ -1,40 +1,41 @@
-import React , { useState } from 'react';
+import React , { useEffect, useState } from 'react';
 
 import ItemCount from '../ItemCount';
+
 import ItemList from '../ItemList/itemL';
 import products from '../products.json'
 
 
 
 const ListContainer = ({geeting}) => {
-   
+  const [list, setlist] = useState([])
 
-  
+   useEffect(() => {
 
-  const promesa = new Promise((resolve, reject) => {
-   
-   
-    let promesa = true;
- 
- 
-    setTimeout(() => {
-    if (promesa) {
+    const getData = new Promise((resolve,) => {
+     
+      setTimeout(() => {
+        
+        resolve(list)
+      }, 2000);
+    })
+      .then((data) => {
+        setlist(products)
+ console.log(data)
+         
+     
+      })
+     
+  }, [])
 
-    resolve(products);
-    } 
-    else {
-    reject('error');
-    }
-    }, 2000);
-    });
 
-  
-return (
-<div>
+
+return (<div>
 
     {geeting}
+  
    
-   <ItemList  items={products}/>
+   <ItemList  items={list}/>
 
     
   <ItemCount stock={5} initial={1} onAdd ={(n)=> alert(`agregado ${n}`) }/>
